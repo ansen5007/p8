72c58ba5d75114f614a0328188724e9709251d0d
Student <Student@PC79>
2016-12-07 12:31:38 +0800

    pppppoooppppp

---
+++ src/NewClass.java
@@ -0,0 +1,13 @@
+
+
+public class NewClass {
+      public static void main (String[] args){
+            int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+                          }
+}
+}
+
